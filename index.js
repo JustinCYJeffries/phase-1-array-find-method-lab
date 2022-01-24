d0fc3aef420b3a33510654a9e38ab47500b611f6
Justin Jeffries <justincyjeffries@gmail.com>
2022-01-23 22:08:56 -0700

    commit tree a98cb9d4a580dc037713a563e6a7752476382c39

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function superbowlWin(array)  {
+    const championship = array.find(array => array.result === "W");
+    if (championship) {
+        return championship.year
+    }
+    else{
+        return undefined
+    }
+}
